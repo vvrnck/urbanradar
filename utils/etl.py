eca11f92cd678457675e3d361fc44c4f3566164f
Gabriel Panza <gabriel@gr4vy.com>
2023-04-11 02:44:52 -0300

    Continuing

--- utils/etl.py
+++ utils/etl.py
@@ -1,91 +1,91 @@
-from shapely.geometry import Point
-import pandas as pd
-import geopandas as gpd
-from geopandas.tools import sjoin
+import psycopg2
+import os
+from sqlalchemy import create_engine, text
+import ast
+import json
 
 
-def find_file_extension(name):
-    try:
-        return name.split('.')[-1]
-    except IndexError:
-        return ''
+def get_connection(database_URI):
+    # Connect to the Postgres database
+    engine = create_engine(database_URI)
+    return engine
 
 
-def from_csv_to_geojson(target_file, col_lat, col_long):
-    target_df = pd.read_csv(target_file)
+def extract_data():
+    # Extract data from the csv file and import into the Postgres database
+    # The csv file is located in the event-file folder
+    # The csv file is named events.csv
+    # The csv file has the following columns:
+    # date, lat, lng
+    # The csv file has the following format:
+    # 2018-01-01, -23.123456, -46.123456
+    database_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    engine = get_connection(database_URI)
+    with engine.connect() as conn:
+        with open('event-file/events.csv', 'r') as f:
+            # Loop through row to find the date and lat and lng in the header
+            row = f.readline().split(',')
+            for ind, r in enumerate(row):
+                if r == 'date':
+                    date_ind = ind
+                if r == 'lat':
+                    lat_ind = ind
+                if r == 'lng':
+                    lng_ind = ind
 
-    data = {'DATAHORA': [], 'lat': [], 'lng': [], 'coordinates': []}
+            # Use schema datalake
+            conn.execute(text("SET search_path TO web"))
 
-    for index, pol in target_df.iterrows():
-        if pol[col_lat] != "IRT" and pol[col_long] != "IRT" and pol[col_lat] != "0" and pol[col_long] != "0":
-            pol[col_lat] = float(pol[col_lat])
-            pol[col_long] = float(pol[col_long])
+            results = conn.execute(text('Select * from "Feature"')).fetchall()
+            coordinates = ast.literal_eval(results[0][5])
+            print(len(coordinates))
+            cleaned_coordinates = '(('
+            for i, row in enumerate(coordinates):
+                # Get the geometry column
+                coords = row
+                lat, long = coords[0], coords[1]
+                cleaned_coordinates += f"{lat} {long}"
+                if i != len(coordinates)-1:
+                    cleaned_coordinates += ", "
+                else:
+                    cleaned_coordinates += "))"
+            print(cleaned_coordinates)
 
-            data['lat'].append(pol[col_lat])
-            data['lng'].append(pol[col_long])
-            data['DATAHORA'].append(pol['DATAHORA'])
-
-    data['coordinates'] = list(zip(data['lat'], data['lng']))
-    target = pd.DataFrame(
-        data, columns=['DATAHORA', 'lat', 'lng', 'coordinates'])
-    # print(target)
-    target['coordinates'] = target['coordinates'].apply(Point)
-    return gpd.GeoDataFrame(
-        target,
-        geometry='coordinates',
-        crs='+init=epsg:4326'
-    )
+            print('Data imported successfully!')
 
 
-def get_centroid():
-    target_df = gpd.read_file('city-hex-polygons-9.geojson')
-    for i in range(len(target_df)):
-        target_df['geometry'][i] = target_df['geometry'][i].centroid
+def open_geojson_and_create_feature():
+    database_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    engine = get_connection(database_URI)
+    with engine.connect() as conn:
+        with open('grid.geojson', 'r') as f:
+            data = json.load(f)
+        conn.execute(text("SET search_path TO web"))
+        for feature in data['features']:
+            # assuming each feature has only one geometry object
+            coordinates = feature['geometry']['coordinates']
+            cleaned_coordinates = '(('
+            list_coordinates = []
+            for i, row in enumerate(coordinates[0]):
+                # Get the geometry column
+                coords = row
+                lat, long = coords[1], coords[0]
+                cleaned_coordinates += f"{lat} {long}"
+                list_coordinates.append([lat, long])
+                if i != len(coordinates[0])-1:
+                    cleaned_coordinates += ", "
+            cleaned_coordinates += "))"
+            coordinates = str(list_coordinates)
+            print(cleaned_coordinates)
+            print(coordinates)
+            conn.execute(text(
+                'INSERT INTO "Feature" (code, tenant_id, layer_id, feature_collection_id, geometry, type, view) VALUES (1, 1, 1, 1, \'{}\', \'POLYGON\', \'{}\')'.format(str(list_coordinates), str(cleaned_coordinates))))
+        conn.commit()
 
-    target_df.to_file("city-hex-polygons-9-centroid.geojson", driver='GeoJSON')
 
-
-def run(target_file, polygonos_file, col_lat, col_long, output_name_file):
-    extension_target_file = find_file_extension(target_file)
-    if extension_target_file == 'csv':
-        print('\t\t- CONVERTENDO ARQUIVO DE EVENTOS PARA O FORMATO GEOJSON')
-        target_gdf = from_csv_to_geojson(target_file, col_lat, col_long)
-    elif extension_target_file == 'geojson':
-        print("\t\t- LENDO ARQUIVO DE EVENTOS")
-        target_gdf = gpd.read_file(target_file)
-    else:
-        raise Exception(
-            '### Error: target file format must be csv or geojson.')
-
-    print('\t\t- LENDO ARQUIVO DE POLIGONOS')
-    polygonos_gdf = gpd.read_file(polygonos_file)
-    print('\t\t- REALIZANDO JUNCAO ENTRE AS LOCALIZACOES')
-    try:
-        teste_df = sjoin(target_gdf, polygonos_gdf, how="left")
-    except AttributeError as err:
-        print("\t\t - Ocorreu um, verifique se spatialindex está instalada.")
-        print("\t\t\t", err)
-        return False
-
-    print(f'\t\t- GERANDO AQRUIVO {output_name_file}')
-    if extension_target_file.lower() == 'geojson':
-        teste_df.to_file(output_name_file, driver='GeoJSON')
-        return True
-    elif extension_target_file.lower() == 'csv':
-        df_out = pd.DataFrame(teste_df).dropna()
-        print(df_out)
-        if not df_out.empty:
-            if "id" in df_out.keys():
-                resp = df_out['id'].str.split('_', expand=True)
-                if len(resp.columns) == 2:
-                    df_out[['x_index', 'y_index']] = resp
-            df_out["DATAHORA"] = df_out["DATAHORA"].astype("string")
-            df_out.to_csv(output_name_file, index=None, header=True)
-            return True
-        else:
-            return False
+def run():
+    open_geojson_and_create_feature()
 
 
 if __name__ == '__main__':
-    run('./event-file/events.csv',
-        'output.geojson', 'lat', 'lng', 'output2.geojson')
+    run()
